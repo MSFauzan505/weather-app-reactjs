b2cc00cfe66871f0e67f6b045eda08aaae092f7a
fauzan <msh.fauzan@gmail.com>
2025-06-27 11:19:00 +0700

    feat: add UI search on searhPage

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import { Outlet } from 'react-router-dom'
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { InputText } from 'primereact/inputtext';
+
 
 const SearchPage = () => {
+  const [value, setValue] = useState('')
   return (
-    <div>SearchPage</div>
+    <div className='flex flex-col'>
+        <InputText id='search' type='text'
+          className="sm:p-inputtext-lg m-auto bg-black/20 text-white 
+          py-2 px-4 w-full sm:w-[500px] mt-3 rounded-xl backdrop-blur-2xl"
+          value={value}
+          placeholder='Search city'
+          onChange={(e) => setValue(e.target.value)} />
+    </div>
   )
 }
 
